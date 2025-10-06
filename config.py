759c9462828618b1f6ddb1ad3eadb4903743e2ef
GTRSADIK <info.gtrbusiness@gmail.com>
2025-10-07 01:10:09 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-API_TOKEN: str = ''  # Your bot token from @BotFather
+API_TOKEN: str = '8117808424:AAExNkuw4qc8Z8OrATkNCT726tQh_r1kREQ'  # Your bot token from @BotFather
 STARS_AMOUNT: int = 1  # Amount in stars (1 - 100000)
